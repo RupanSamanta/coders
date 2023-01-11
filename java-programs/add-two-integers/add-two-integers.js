878fc4a7d6a470967bef88e3194abfd638c96887
Rupan Samanta <96118309+RupanSamanta@users.noreply.github.com>
2023-01-11 22:07:34 +0530

    Add files via upload

---
+++ java-programs/add-two-integers/add-two-integers.js
@@ -0,0 +1,18 @@
+const CODE = 
+`<code>import java.util.Scanner;</code>` +
+`<code>public class Add_Two_Integers {</code>` +
+`<code>   public static void main(String args[]) {</code>` +
+`<code>      Scanner sc = new Scanner(System.in);</code>` +
+`<code>      System.out.print("Enter First Number: ");</code>` +
+`<code>      int num1 = sc.nextInt();</code>` +
+`<code>      System.out.print("Enter Second Number: ");</code>` +
+`<code>      int num2 = sc.nextInt();</code>` +
+`<code>      int sum = num1 + num2;</code>` +
+`<code>      System.out.println("Sum = " + sum);</code>` +
+`<code>   }</code>` +
+`<code>}</code>`,
+
+OUTPUT = 
+`<code>Enter First Number: 20</code>
+<code>Enter Second Number: 30</code>
+<code>Sum = 50</code>`;
