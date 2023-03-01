9b47aa0092fd5f7f04d67b5ec9be3c9128a9bb00
Rupan Samanta <96118309+RupanSamanta@users.noreply.github.com>
2023-03-01 13:30:44 +0530

    Update Add Two Integers.java

--- Java/Add Two Integers.java
+++ Java/Add Two Integers.java
@@ -1,12 +1,12 @@
-import java.util.Scanner;
-public class Add_Two_Integers {
-   public static void main(String args[]) {
-      Scanner sc = new Scanner(System.in);
-      System.out.print("Enter First Number: ");
-      int num1 = sc.nextInt();
-      System.out.print("Enter Second Number: ");
-      int num2 = sc.nextInt();
-      int sum = num1 + num2;
-      System.out.println("Sum = " + sum);
-   }
+import java.util.Scanner;//n
+public class Add_Two_Integers {//n
+   public static void main(String args[]) {//n
+      Scanner sc = new Scanner(System.in);//n
+      System.out.print("Enter First Number: ");//n
+      int num1 = sc.nextInt();//n
+      System.out.print("Enter Second Number: ");//n
+      int num2 = sc.nextInt();//n
+      int sum = num1 + num2;//n
+      System.out.println("Sum = " + sum);//n
+   }//n
 }
