e83a628239112e8ae483b826816a8805709a6b6b
Rupan Samanta <96118309+RupanSamanta@users.noreply.github.com>
2023-03-01 12:23:59 +0530

    Add files via upload

---
+++ java-programs/programs.js
@@ -0,0 +1,18 @@
+const PROGRAM = 
+[
+   {
+      title: "Java Program to Accept and Print the Sum of Two Integers",
+      type: "Java Operator",
+      url: "add-two-integers"
+   },
+   {
+      title: "Java Program to Accept the Size and the Elements of a Matrix and Display It",
+      type: "Java Double Dimensional Array",
+      url: "accept-display-matrix"
+   },
+   {
+      title: "Java Program to Accept the Size and the Elements of a Square Matrix and Display Sum of the Diagonals",
+      type: "Java Double Dimensional Array",
+      url: "array-diagonal-sum"
+   }
+]
